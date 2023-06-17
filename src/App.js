4c6a785ce582c3fda890fb9853dc054b64047a50
Mansi Parmar <mansipa34@gmail.com>
2023-06-18 01:27:14 +0530

    Add to-do-list

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import './App.css';
+import MyForm from './Form';
+import Todo from './Todo';
+
+function App() {
+  return (
+    <>
+      <MyForm />
+      <Todo />
+    </>
+  );
+}
+
+export default App;
